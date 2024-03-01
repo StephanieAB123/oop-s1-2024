49dc8144462c40690bfd1a4419e0d327b96ba9df
StephanieAB123 <a1877567@adelaide.edu>
2024-03-01 14:59:37 +1030

    Testing

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -4,13 +4,12 @@ using namespace std;
 //first sort the array
 //find the half == to the median
 // bubble sort
-//https://prepinsta.com/data-structures-algorithms/sorting-of-array/ 
-
+//https://prepinsta.com/data-structures-algorithms/sorting-of-array/
 int median_array(int array[], int n){
 
     if(n > 1){
         for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
+            for(int j = 0; j < n; j++){
                 if(array[i] > array[j]){
                     int temp = array[i];
                     array[i] = array[j];
