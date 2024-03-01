28b0e7395d8a9df63f59b58299808e7b03f353c8
StephanieAB123 <a1877567@adelaide.edu>
2024-03-01 15:19:47 +1030

    testing

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -11,5 +11,11 @@ bool is_fanarray(int array[], int n){
         }
     }
 
+    for(int i = 0; i < n/2; i++){
+        if(array[i] > array[i+1]){
+            return false;
+        }
+    }
+
     return true;
 }
