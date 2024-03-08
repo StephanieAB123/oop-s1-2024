1c9b472c44d4b4d34626ba869306ebd647d79ce0
StephanieAB123 <a1877567@adelaide.edu>
2024-03-08 15:35:13 +1030

    testing

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -3,17 +3,12 @@ using namespace std;
 
 int is_identity(int array[10][10]){
 
-    int flag = 0;
+    int flag = 1;
 
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
-            if((array[i][j] != 1) && (array[i][j] != 0)){
-                return 0;
-            }
-            if(array[i] == array[j] != 1){
-                flag = 0;    
-            }else{
-                flag = 1;
+            if((array[i][j] != 1) && (array[j][i] != 0)){
+                flag = 0;
             }
         }
     }
