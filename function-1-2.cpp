a2d6a297d947a92acd8ea26b5f7bca6ce7a61d52
StephanieAB123 <a1877567@adelaide.edu>
2024-03-08 15:30:59 +1030

    testing

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -8,7 +8,7 @@ int is_identity(int array[10][10]){
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             if((array[i][j] != 1) && (array[i][j] != 0)){
-                return;
+                return 0;
             }
             if(array[i] == array[j] == 1){
                 flag = 1;    
