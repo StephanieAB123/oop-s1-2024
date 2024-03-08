0b02fc50526213fa352044e39f8bf3070175abed
StephanieAB123 <a1877567@adelaide.edu>
2024-03-08 16:18:28 +1030

    testing

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,4 +1,22 @@
 #include <iostream> 
+#include <string> 
 using namespace std; 
 
-void print_binary_str(string decimal_number);
+void print_binary_str(string decimal_number){
+
+    int decimal = stoi(decimal_number);
+    int remainder; 
+    int binary = 0; 
+    int i = 1; 
+
+    while(decimal != 0){
+        remainder = decimal%2; 
+        decimal = decimal/2; 
+        binary = binary + remainder*i; 
+        i = i*10; 
+    }
+
+    cout << binary << endl;
+
+}
+
---
+++ function-2-2.cpp
@@ -0,0 +1,7 @@
+#include <iostream> 
+using namespace std; 
+
+int binary_to_int(int binary_digits[], int number_of_digits){
+
+    
+}
