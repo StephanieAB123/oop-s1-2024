bf54e800035027fbae737c3d31c3be7298af7aa2
StephanieAB123 <a1877567@adelaide.edu>
2024-03-22 23:57:24 +1030

    testing

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,21 +1,13 @@
-#include <iostream> 
+#include <iostream>
+#include <string>
 #include "Person.h"
-using namespace std; 
 
-extern PersonList createPersonList(int n);
 
-int main(){
-    int n; 
-    cout << "Enter a number: ";
-    cin >> n;
+using namespace std;
 
-    PersonList pl = createPersonList(n);
-    cout << "Person List --- Number of People: " << pl.numPeople << endl;
+extern PersonList deepCopyPersonList(PersonList);
 
-    for(int i = 0; i < n; i++){
-        cout << "Person " << i + 1 << " --- Name: " << pl.people[i].name << " --- Age: " << pl.people[i].age << endl; 
-    }
+int main()
+{
 
-    delete[] pl.people;
-    return 0;
 }
--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,33 +1,13 @@
-#include <iostream> 
+#include <iostream>
 #include <string>
 #include "Person.h"
+
+
 using namespace std;
 
-extern PersonList deepCopyPersonList(PersonList pl);
+extern PersonList deepCopyPersonList(PersonList);
 
-int main(){
-    PersonList pl;
-    pl.numPeople = 2; 
-    pl.people = new Person[pl.numPeople];
-    pl.people[0] = "Paige";
-    pl.people[1] = "Pie";
-
-    PersonList newPl = deepCopyPersonList(pl);
-    cout << "Original" << endl;
-    for(int i = 0; i < pl.numPeople; i++){
-        cout << pl.people[i].name << ", " << pl.people[i].age << endl;
-    }
-
-    cout << "Copied" << endl;
-    for(int i = 0; i < pl.numPeople; i++){
-        cout << newPl.people[i].name << ", " << newPl.people[i].age << endl;
-    }
-
-    delete[] pl.people;
-    delete[] newPl.people;
-
-    return 0;
-
-
+int main()
+{
 
 }
